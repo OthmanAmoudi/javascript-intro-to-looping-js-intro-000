d0fa997fca8dcf9d0a840b1fc9f0cacfacf08fb3
Othman AMD <red.fantastic@hotmail.com>
2017-09-18 18:33:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,15 @@ function whileLoop(n){
   }
   return "done";
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  }
+
+  do {
+    array.slice(1);
+  } while(array.length == 0 && maybeTrue())
+
+  return array;
+}
